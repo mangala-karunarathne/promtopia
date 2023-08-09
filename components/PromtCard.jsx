8c155fa559903574d7ab589be52b5b78157a77bc
mangala-karunarathne <mangalaicc@gmail.com>
2023-08-09 17:14:14 +0530

    FIX | Setup issues and Add Components

---
+++ components/PromtCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PromtCard() {
+  return (
+    <div>PromtCard</div>
+  )
+}
+
+export default PromtCard
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,14 +16,5 @@ module.exports = {
       }
     },
   },
-
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.jsx',
-    './src/**/*.ts',
-    './src/**/*.tsx',
-  ],
-
   plugins: [],
 }
