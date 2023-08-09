9426e0c873f594bf40d9ebaf3e2092850d6d5521
mangala-karunarathne <mangalaicc@gmail.com>
2023-08-09 12:19:33 +0530

    CHG | Home Page

--- app/page.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <section className="w-full flex-center flex-col">
-
-    </section>
-  );
-};
-
-export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,5 +16,14 @@ module.exports = {
       }
     },
   },
+
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+  ],
+
   plugins: [],
 }
